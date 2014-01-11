b45361889904c412520b4004817ee8275ec81902
Karen Ng <karenyng@ucdavis.edu>
2014-01-11 14:47:44 -0800

    ok now really fixed the off by 1 bug

--- freq_count.sh
+++ freq_count.sh
@@ -9,7 +9,6 @@ fileno=${#files[*]}
 
 for ((j=0; j < ${fileno}; j++));
 do 
-
 	arr=($(head -1 ${files[$j]} | tr "," "\n"))
 	arrLen=${#arr[*]}
 	col=-99
@@ -19,10 +18,11 @@ do
 		x=${arr[$i]}
 		if [ $x == "ArrDelay" ] || [ $x == "\"ARR_DELAY\"" ]; then 
 			# definition of column count is off by 1 between bash array and cut 
-			(( col +=$i ))
+			col=$i
+			(( col += 1 ))
 			break
 		fi
 	done
 
-	time cut -d',' -f${col} ${files[$j]} | sort -n | uniq -c > $outdir/${j}.txt 
+	time cut -d',' -f${col} ${files[$j]} | sort -n | uniq -c > ${j}.txt 
 done
