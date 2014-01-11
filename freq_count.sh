4591ba46afdadb57c5609b875b95aa7bd880faad
Karen Ng <karenyng@ucdavis.edu>
2014-01-11 14:27:37 -0800

    fixed off by one error, now loop bounds / column counts are all correct

--- freq_count.sh
+++ freq_count.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 shopt -s nullglob
-dir="./data"
+dir="./data_subset"
+out_dir="./freq_count_data"
 # store all file names in a bash array
 files=( "$dir"/* ) 
 fileno=${#files[*]}
 
-for ((j=1; j<${fileno}; j++));
+for ((j=0; j < ${fileno}; j++));
 do 
 
 	arr=($(head -1 ${files[$j]} | tr "," "\n"))
 	arrLen=${#arr[*]}
-
 	col=-99
-	for ((i=1; i <= ${arrLen}; i++)); 
+
+	for ((i=0; i < ${arrLen}; i++)); 
 	do 
 		x=${arr[$i]}
 		if [ $x == "ArrDelay" ] || [ $x == "\"ARR_DELAY\"" ]; then 
-			col=$i
+			# definition of column count is off by 1 between bash array and cut 
+			(( col +=$i ))
 			break
 		fi
 	done
-	echo "${files[$j]} $col"
-	## time cut -d',' -f${col} ${1} | uniq -c  
+
+	time cut -d',' -f${col} ${files[$j]} | sort -n | uniq -c > $outdir/${j}.txt 
 done
