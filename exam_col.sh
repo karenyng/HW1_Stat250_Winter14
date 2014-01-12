a61e148120dee4ac9de9c39f5f90972bcd909494
Karen Ng <karenyng@ucdavis.edu>
2014-01-12 08:36:50 -0800

    update to approach

--- exam_col.sh
+++ exam_col.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#---------------------------------------------------------------------------
+#------------------------------------------------------------
 # Author: Karen Ng
 # This script 
 # * reads in airline csv data files 
@@ -8,11 +8,11 @@
 # * cut the column then count the frequencies of a certain delay time
 # * returns frequency count in files named 1.txt 2.txt etc. 
 # * the frequency count files still has header and NA values mixed inside
-# * have to use R or other language to discard those 
-#---------------------------------------------------------------------------
+# * have to use R or other language to descard those 
+#------------------------------------------------------------
 
 shopt -s nullglob
-dir="./data"
+dir="./data_subset"
 out_dir="./freq_count_data"
 # store all file names in a bash array
 files=( "$dir"/* ) 
@@ -34,35 +34,13 @@ do
 
 			# if it is a monthly csv, have to add two to the column count for the
 			# column value to be fetched correctly by cut. 
-			# This is because some values in the monthly CSV contain commas
 			if [ $x == "\"ARR_DELAY\"" ]; then 
 				(( col += 2 ))
 			fi
-
 			break
 		fi
 	done
 
-	echo "Writing the file-$j delay time to freq_count.txt"
-	# store the frequency count of all the csv files in one single file
-	# possible time improvement: 
-	# use an array to hold these instead of writing it to a file first 
-	# the col-finding and the writing out takes around 1 min 8 s real time  
-	if [ $j -eq 0 ]; then 
-		cut -d',' -f${col} ${files[$j]} > freq_count.txt 
-	else 
-		cut -d',' -f${col} ${files[$j]} >> freq_count.txt 
-	fi
+	cut -d',' -f${col} ${files[$j]} > col${j}.txt 
 
 done
-
-echo "starting to sort all the frequency to sorted_freq.txt"
-# real	3m14.067s
-# user	3m7.896s
-# sys	0m5.388s
-time cat freq_count.txt | sort -n | uniq -c > sorted_freq.txt 
-
-# Overall runtime 
-# real	4m22.940s
-# user	4m6.412s
-# sys	0m11.068s
