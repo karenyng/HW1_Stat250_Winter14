14ab46e2f2f5148a77b5b82e6778f98d18fae27c
karenyng <kyyng@ucdavis.edu>
2014-01-15 10:29:23 -0800

    fix possible bug

--- freq_count.sh
+++ freq_count.sh
@@ -66,10 +66,11 @@ done
 # use sed to remove the trailing decimal places for some delay time entries
 # sort them
 # find unique counts 
-# remove header --- could have just used "grep -v "  
-cat freq_count.txt | sort -n | uniq -c |\
+# remove header --- could have just used "grep -v " 
+cat freq_count.txt | sed -E 's/([0-9]+)\.00/\1/g' |\ 
+	sort -n |\
+	uniq -c |\
    	sed -e '/ArrDelay/d' -e '/ARR_DEL15/d' |\
-	sed -E 's/([0-9]+)\.00/\1/g' 
 
 # can output to a text file to check values 
 #> sorted_freq.txt
